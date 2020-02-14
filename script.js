04b245cd03d9fd2ed74adf1ac8a62c8bbc673eeb
James <jtsetsekas@gmail.com>
2020-02-14 03:49:01 -0500

    Update Homepage
    Update Homepage

--- script.js
+++ script.js
@@ -1,115 +1,65 @@
-let canvas = document.getElementById("canvas"),
-    ctx = canvas.getContext("2d"),
-	w,
-	midX,
-	h,
-	particles = [];
+$(function() {
+  $('.js-nav a, .js-connect').click(function(e) {
+    e.preventDefault();
+    $('body, html').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 750);
+  });
+});
 
-const Tau = Math.PI * 2,
-	  ConnectionDist = 100, 
-	  maxParticles = 100, 
-	  radius = 3,
-      Msqrt = Math.sqrt,
-      Mrandom = Math.random;
+var TxtType = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
 
-function handleResize(){
-	w = ctx.canvas.width = window.innerWidth;
-	h = ctx.canvas.height = window.innerHeight;
-	midX = w * .5;
-}
-window.onresize = () => handleResize();
-handleResize();
+TxtType.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
 
-function createParticles() {
-	let vRange = 1.5,
-		vMin = .5,
-		vx, vy;
-	for (let i = 0; i < maxParticles; i++) {
-		vx = Mrandom() * vRange + vMin;
-		vy = Mrandom() * vRange + vMin;
-		if(Mrandom() > .5){ vx *= -1; }
-		if(Mrandom() > .5){ vy *= -1; }
-		particles.push({
-			x: Mrandom() * w - radius,
-			y: Mrandom() * h - radius,
-			xv: Mrandom() * vx,
-			yv: Mrandom() * vy,
-			strokeColour: {h:0, s:1}
-		});
-	}
-}
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
 
-function update() {
-	let p;
-	for (let loop = particles.length, i = 0; i < loop; i++) {
-		p = particles[i];
-		// move
-		p.x += p.xv;
-		p.y += p.yv;
-		// keep in bounds
-		if (p.x < 0) {
-			p.x = 0;
-			p.xv *= -1;
-		}
-		else if (p.x > w) {
-			p.x = w;
-			p.xv *= -1;
-		}
-		if (p.y < 0) {
-			p.y = 0;
-			p.yv *= -1;
-		}
-		else if (p.y > h) {
-			p.y = h;
-			p.yv *= -1;
-		}
-	}
-}
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
-function connect(){
-	let p1, p2;
-	for (let i = 0; i < maxParticles-1; i++) {
-		p1 = particles[i];
-		for (let j = i + 1; j < maxParticles; j++) {
-			p2 = particles[j];
-			currentDist = getDistance(p2.x, p1.x, p2.y, p1.y);
-			if (currentDist < ConnectionDist) {
-				ctx.beginPath();
-				ctx.moveTo(p1.x, p1.y);
-				ctx.strokeStyle = 'hsla(' + p1.hue + ', 50%, 50%, ' + (1 - currentDist * 0.01) + ')';
-				ctx.lineTo(p2.x, p2.y, p1.x, p1.y);
-				ctx.stroke();
-			}
-		}
-	}
-}
+  var that = this;
+  var delta = 200 - Math.random() * 100;
 
-function draw() {
-	let p, d;
-	for (let loop = particles.length, i = 0; i < loop; i++) {
-		p = particles[i];
-		d = getDistance(midX, p.x, h, p.y);
-		p.hue = d;
-		ctx.beginPath();
-		ctx.fillStyle = 'hsla(' + d + ' , 50%, 30%, 1)';
-		ctx.arc(p.x, p.y, radius, 0, Tau);
-		ctx.fill();
-	}
-}
+  if (this.isDeleting) { delta /= 2; }
 
-function getDistance(x1, x2, y1, y2) {
-	let a = x1 - x2,
-		b = y1 - y2;
-	return Msqrt(a * a + b * b);
-}
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
 
-function animate() {
-	canvas.width = w;
-	update();
-	connect();
-	draw();
-	requestAnimationFrame(animate);
-}
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
 
-createParticles();
-animate();
+window.onload = function() {
+  var elements = document.getElementsByClassName('typewrite');
+  for (var i=0; i<elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-type');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  document.body.appendChild(css);
+};
